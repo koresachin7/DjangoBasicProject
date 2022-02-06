9005a1a8e4cd579571c46f2f9f4a151b7a93acf2
koresachin7 <koresachin7@gmail.com>
2022-02-06 08:33:34 +0530

    Modify: Adding class for verify toke method and change name of utility file class

--- fundoonotes/notes_app/urls.py
+++ fundoonotes/notes_app/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+# from django.conf.urls import url
+# from rest_framework_swagger.views import get_swagger_view
+#
+# schema_view = get_swagger_view(title='Pastebin API')
 
 from . import views
 
 # from .views import UserRegistration, Login,LoginSerializer,UserDetail
 
 urlpatterns = [
-    path('notes_api_view', views.NotesAPIView.as_view(), name='notes_crud')
+    path('notes_api_view', views.NotesAPIView.as_view(), name='notes_crud'),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
--- fundoonotes/users/utility.py
+++ fundoonotes/users/utility.py
@@ -1,13 +1,23 @@
 import jwt, datetime
 
 
-class UserViwe:
+class JwtEnodeDecode:
     @staticmethod
     def encode(payload):
+        """
+        Description:
+                This method is writing for encode data
+        :return : Token
+        """
         token = jwt.encode(payload, 'secret', algorithm='HS256')
         return token
 
     @staticmethod
     def decode(token_encode):
+        """
+        Description:
+                    This method is writing for decode data
+        :return : payload
+        """
         payload = jwt.decode(token_encode, 'secret', algorithms=['HS256'])
         return payload
